00649077f62dfcf9fea1dd659bbc2ee914fdbb40
Sukhveer7247 <127840683+Sukhveer7247@users.noreply.github.com>
2023-03-18 11:44:59 +0530

    IPL GITHUB ASSIGNMENTS
    SUBMISSION OF ASSIGNMENTS

--- Assignment6.c
+++ Assignment6.c
@@ -1,6 +1,18 @@
 //Write a C code to find the number of distinct elements in a sorted array.
 
-int main()
+#include <stdio.h>
+int main() 
 {
+    int arr[] = {1, 2, 3, 3, 4, 4, 4, 5, 6, 6, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int distinct_count = 0;
+    for (int i = 0; i < n; i++) {
+        if (i == 0 || arr[i] != arr[i-1]) {
+            distinct_count++;
+        }
+    }
+
+    printf("The number of distinct elements is: %d\n", distinct_count);
+
   return 0;
 }
--- Program1.c
+++ Program1.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main() 
 {
-    
+    printf("i am sukhveer singh (2210997247)")
     return 0;
 }
 
